24e96a86225a1256a38637551651f00428e68337
ogu_cp <SND\ogu_cp>
2014-04-05 18:55:36 +0000

    database Selection for backup functionality

---
+++ MongoBackupManager/MongoBackupManager/ViewModels/DatabaseVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MongoBackupManager
+{
+    public class DatabaseVM : VMBase
+    {
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (value != _isSelected)
+                {
+                    _isSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private DateTime _createdDate;
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set
+            {
+                if (value != _createdDate)
+                {
+                    _createdDate = value;
+                    NotifyPropertyChanged("CreatedDate");
+                    NotifyPropertyChanged("CreatedDateStr");
+                }
+            }
+        }
+        public string CreatedDateStr
+        {
+            get { return _createdDate.ToString("dd.MM.yyyy HH:mm"); }
+        }        
+    }
+}
